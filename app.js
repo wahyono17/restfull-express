68a2b6e8d6cb58166a47392892f4cfc4a219761c
wahyono17 <wahyono17@gmail.com>
2020-07-12 22:18:38 +0700

    finish request body

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const app = express(); //execute express spt fungsi, tanda () berarti telah di exekusi
-const morgan = require('morgan');//morgan akan menangasi next id midleware
+const morgan = require('morgan');//ini untuk logger
+const bodyParser = require("body-parser"); //untuk menangkap http request dari http
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 const { use } = require('./api/routes/products');
 
-//app.use(morgan('dev'));
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//ini untuk menonactive kan CORS, karena pasti beda server antara client dan server api
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  //semua method post dan put pasti mengirim options dulu sebelum mengirim method
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      return res.status(200).json({});
+  }
+  next();
+});
 
 app.use('/products', productRoutes); //semua ke /product akan di lempar ke productRoutes
 app.use('/orders', orderRoutes);
